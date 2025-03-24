2a2112be48c7542165dd3bb97393f1b6286f6095
Mahiii <mygamming436@gmail.com>
2025-03-25 01:32:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ SILICON_PIC = os.environ.get("SILICON_PIC", "https://files.catbox.moe/qzg4n0.jpg
 API_ID = int(getenv("API_ID", "21316774"))
 API_HASH = str(getenv("API_HASH", "50f64c71c78a34a85dad33fb78fc6c33"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", "7508114207:AAHSgrtvNsFhulIdFVQczsna_Saz9vUSKY4"))
-FORCE_SUB = os.environ.get("FORCE_SUB", "Drama_hindi_dub") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1002408300116") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://Filterbot:wywgsgwwwuww2iqiw@cluster0.s7cys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
